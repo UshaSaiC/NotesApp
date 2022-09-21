045cd2925fc3d668bcce7631913e87518a97a113
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-21 18:50:01 +0530

    The notes are displayed on watch as soon as they are added

--- NotesApp Watch App/View/ContentView.swift
+++ NotesApp Watch App/View/ContentView.swift
@@ -11,8 +11,32 @@ struct ContentView: View {
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
     func save(){
-        dump(notes)
+        // dump(notes)
+        do{
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        }catch{
+            print("Saving data has failed")
+        }
+    }
+    
+    func load(){
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch{
+                // do nothing
+            }
+        }
     }
     
     var body: some View {
@@ -45,8 +69,22 @@ struct ContentView: View {
                 .navigationBarTitle("Notes")
             }
             Spacer()
-            Text("\(notes.count)")
+            List {
+                ForEach(0..<notes.count, id: \.self) { index in
+                    HStack{
+                        Capsule()
+                            .frame(width: 4)
+                            .foregroundColor(.accentColor)
+                        Text(notes[index].text)
+                            .lineLimit(1)
+                            .padding(.leading, 5)
+                    }
+                }
+            }
         }
+        .onAppear(perform: {
+            load()
+        })
     }
 }
 
