72094c7a5032721e302ce2226e50050d39739206
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-21 13:55:51 +0530

    Initial Commit

---
+++ NotesApp Watch App/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp Watch App
+//
+//  Created by Usha Sai Chintha on 21/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
